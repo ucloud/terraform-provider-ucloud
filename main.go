f5a0890b66644e8a76c0c55b37ad2dacdd65d87c
cgriggs01 <cgriggs@hashicorp.com>
2018-12-19 12:30:21 -0800

    final changes for release

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/ucloud/terraform-provider-ucloud/ucloud"
+	"github.com/terraform-providers/terraform-provider-ucloud/ucloud"
 )
 
 func main() {
