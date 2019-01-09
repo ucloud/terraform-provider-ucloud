b4b96eae087e18f401963265efecb25ff8330adc
mingsheng.su <shawn.mssu@gmail.com>
2019-01-09 18:57:57 +0800

    fix parse instance type test

--- ucloud/types_test.go
+++ ucloud/types_test.go
@@ -20,7 +20,7 @@ func Test_parseInstanceType(t *testing.T) {
 		{"ok_basic", args{"n-basic-1"}, &instanceType{1, 2048, "n", "basic"}, false},
 		{"ok_standard", args{"n-standard-1"}, &instanceType{1, 4096, "n", "standard"}, false},
 		{"ok_highmem", args{"n-highmem-1"}, &instanceType{1, 8192, "n", "highmem"}, false},
-		{"ok_customized", args{"n-customized-1-1"}, &instanceType{1, 1024, "n", "customized"}, false},
+		{"ok_customized", args{"n-customized-1-5"}, &instanceType{1, 5120, "n", "customized"}, false},
 
 		{"err_type", args{"nx-highcpu-1"}, nil, true},
 		{"err_scale_type", args{"n-invalid-1"}, nil, true},
@@ -29,6 +29,7 @@ func Test_parseInstanceType(t *testing.T) {
 		{"err_cpu_is_invalid", args{"n-highcpu-x"}, nil, true},
 		{"err_customized_format_len", args{"n-customized-1"}, nil, true},
 		{"err_customized_format_number", args{"n-customized-x"}, nil, true},
+		{"err_customized_should_be_standard", args{"n-customized-1-2"}, nil, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
