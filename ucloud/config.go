171c7b50b6ace49d714e35112b7671179dedb271
mingsheng.su <shawn.mssu@gmail.com>
2020-04-15 12:04:21 +0800

    upate config

--- ucloud/config.go
+++ ucloud/config.go
@@ -53,7 +53,7 @@ func (c *Config) Client() (*UCloudClient, error) {
 	// enable auto retry with http/connection error
 	cfg.MaxRetries = c.MaxRetries
 	cfg.LogLevel = log.PanicLevel
-	cfg.UserAgent = "Terraform-UCloud/1.18.0"
+	cfg.UserAgent = "Terraform-UCloud/1.19.0"
 	cfg.BaseUrl = c.BaseURL
 
 	if isAcc() {
