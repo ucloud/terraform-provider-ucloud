cc09a80eeee3d1bbf78a8a8d0c82e04a15b4d819
mingsheng.su <mingsheng.su@ucloud.cn>
2020-09-07 23:00:48 +0800

    terraform fmt

--- examples/vpc/main.tf
+++ examples/vpc/main.tf
@@ -4,7 +4,7 @@ provider "ucloud" {
 }
 
 provider "ucloud" {
-  alias = "shanghai"
+  alias  = "shanghai"
   region = var.peer_region
 }
 
@@ -19,14 +19,14 @@ resource "ucloud_vpc" "foo" {
 }
 
 resource "ucloud_vpc" "bar" {
-  provider = ucloud.shanghai
+  provider    = ucloud.shanghai
   name        = "tf-example-vpc-02"
   tag         = "tf-example"
   cidr_blocks = ["10.10.0.0/16"]
 }
 
 resource "ucloud_vpc_peering_connection" "connection" {
-  depends_on = [ucloud_udpn_connection.default]
+  depends_on  = [ucloud_udpn_connection.default]
   vpc_id      = ucloud_vpc.foo.id
   peer_vpc_id = ucloud_vpc.bar.id
   peer_region = var.peer_region
